0b5ec0ba5bc626bda665682882ea724088f378bc
d0bhareach <viacheslav.modestov@gmail.com>
2023-03-20 11:36:02 +0100

    Updates

---
+++ static/js/353.8a5a2a7b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront=self.webpackChunkfront||[]).push([[353],{353:function(n,t,e){e.r(t),e.d(t,{default:function(){return i}});var s=e(184);function i(){return(0,s.jsxs)("div",{children:[(0,s.jsx)("h3",{children:"This is First Sub Component."}),"This component is subling to Second Sub Component."]})}}}]);
+//# sourceMappingURL=353.8a5a2a7b.chunk.js.map
---
+++ static/js/543.6d52a876.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront=self.webpackChunkfront||[]).push([[543],{543:function(n,t,e){e.r(t),e.d(t,{default:function(){return i}});var s=e(184);function i(){return(0,s.jsxs)("div",{children:[(0,s.jsx)("h3",{children:"This is Second Sub Component."}),"This component is subling to First Sub Component."]})}}}]);
+//# sourceMappingURL=543.6d52a876.chunk.js.map
